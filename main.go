57ec3ad0d039502a282da0fc6532eaf607944b40
luoying-zero <3348015570@qq.com>
2025-03-21 00:37:13 +0800

    Update 1 file: main.go

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
         MaxIdleConns:        0,          // 全局最大空闲连接数
         MaxIdleConnsPerHost: 2^63-1,           // 每个主机的最大空闲连接数
         MaxConnsPerHost:    0,
-        TLSHandShakeTimeout:     20,
+        TLSHandshakeTimeout:     20,
         ForceAttemptHTTP2:  true,
     }
     
